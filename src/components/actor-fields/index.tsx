37985a10d2ff670bfca0e6188afb7097915bacce
Riandy Dimas <riandy.dimas@live.com>
2024-08-16 18:32:32 +0700

    feat: add dummy for styling field list (#1)

--- src/components/actor-fields/index.tsx
+++ src/components/actor-fields/index.tsx
@@ -1,7 +1,153 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const ActorFields = () => {
-  return <div>List of actor fields...</div>
+  return (
+    <div className="grid grid-flow-row h-full gap-2">
+      <ul className="list-none overflow-auto gap-2 grid grid-flow-row">
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nama
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Jabatan
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            Nomor Induk Pegawai
+          </Button>
+        </li>
+        <li>
+          <Button variant="outline" className="w-full">
+            NUPTK
+          </Button>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default ActorFields
